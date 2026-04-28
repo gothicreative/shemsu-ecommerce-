a0b2e079fc0dbcec9a1e6f0bac6b1cb711ea025a
hafiz adem <huhuhudadada393@gmail.com>
2026-04-28 09:33:57 +0300

    try separately

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,20 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+// 	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+// 	withCredentials: true,
+// });
+
+// export default axiosInstance;
+
 import axios from "axios";
+
+const fallbackBaseURL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+const apiBaseURL = import.meta.env.VITE_API_URL || fallbackBaseURL;
 
 const axiosInstance = axios.create({
 	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL: apiBaseURL,
 	withCredentials: true,
 });
 
